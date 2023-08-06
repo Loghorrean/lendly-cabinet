d2dd0ea89143a11b169f7dfc29e33d5432e96c9e
Loghorrean <loghorrean74@gmail.com>
2023-08-06 18:43:07 +0300

    Started making transactions

--- app/(main)/(protected)/(dashboard)/reports/transactions/page.tsx
+++ app/(main)/(protected)/(dashboard)/reports/transactions/page.tsx
@@ -1,3 +1,10 @@
+import styles from "./styles.module.scss";
+import ExternalTransactionsBlock from "@/src/widgets/reports/transactions/ExternalTransactionsBlock";
+
 export default function TransactionsReports() {
-    return <div>Transactions Reports</div>;
+    return (
+        <div className={styles.transactions_reports}>
+            <ExternalTransactionsBlock />
+        </div>
+    );
 }
